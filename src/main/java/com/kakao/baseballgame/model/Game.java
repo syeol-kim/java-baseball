b6ff4870d0e7fbecfcc503a02bec011440f56349
syeol-kim <syeol.kim@daum.net>
2020-12-31 21:17:18 +0900

    fix: implement game
    Implement key business logic that controls the game.

--- src/main/java/com/kakao/baseballgame/model/Game.java
+++ src/main/java/com/kakao/baseballgame/model/Game.java
@@ -11,6 +11,20 @@ public class Game {
     final int TEN = 10;
 
     Integer[] answer;
+    int state;
+
+    public Game(){
+        createAnswer();
+        setState(GameState.PLAYING.ordinal());
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
 
     public Integer[] getAnswer() {
         return answer;
@@ -20,6 +34,16 @@ public class Game {
         if(answer.size() == NUM_DIGITS){
             this.answer = answer.toArray(new Integer[NUM_DIGITS]);
         }
+    }
+
+    public Pair<Integer, Integer> play(int number){
+        Integer[] digits = numberToArray(number);
+        return createHint(digits);
+    }
+
+    public void goNext(int option){
+        setState((option == GameOption.RESTART.ordinal()) ? GameState.PLAYING.ordinal() : GameState.FINISHED.ordinal());
+        createAnswer();
     }
 
     public void createAnswer(){
@@ -51,7 +75,7 @@ public class Game {
         return new Pair<>(numStrikes, numBalls);
     }
 
-    private Integer[] toArray(int number){
+    private Integer[] numberToArray(int number){
         Integer[] array = new Integer[NUM_DIGITS];
         for(int i = 0, j = (int) Math.pow(TEN, NUM_DIGITS-1); number!=0; ++i){
             array[i] = number/j;
