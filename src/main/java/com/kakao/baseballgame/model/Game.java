79fd25a5e7447856f5e4e5eda2f9e2d94d903f2d
syeol-kim <syeol.kim@daum.net>
2020-12-31 19:01:35 +0900

    fix: make 'NUM_DIGITS' public

--- src/main/java/com/kakao/baseballgame/model/Game.java
+++ src/main/java/com/kakao/baseballgame/model/Game.java
@@ -5,8 +5,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class Game {
+    public static final int NUM_DIGITS = 3;
     final int TEN = 10;
-    static final int NUM_DIGITS = 3;
 
     Integer[] answer;
 
